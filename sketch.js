da7015bb725e7d20f4042c252682140d83bbfb3e
Shubham1618 <76488872+Shubham814@users.noreply.github.com>
2021-06-16 02:38:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function preload(){
   player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
   fruitImg = loadImage("banana.png");
   stoneImg = loadImage("stone.png");
-  gameOverImg = loadImage("gameover.png");
+  gameOverImg = loadImage("gameOver.png");
 }
 
 function setup() {
